6016ed5798661ec637d36e1599a13a36632c35cc
Karel Wintersky <karel.wintersky@yandex.ru>
2021-06-10 13:07:21 +0300

    1.35.1
    * [*] Path fixes

--- sources/Path.php
+++ sources/Path.php
@@ -16,7 +16,7 @@ class Path
      */
     public static function create($path)
     {
-        return new self($path);
+        return new self($path, false);
     }
     
     public static function createImmutable($path)
@@ -34,6 +34,8 @@ class Path
         } elseif (is_array($path)) {
             $this->atoms = $path;
         }
+        
+        $this->atoms = $this->trimEach($this->atoms);
     }
     
     public function toString()
@@ -46,6 +48,7 @@ class Path
         if (is_string($data)) {
             $data = explode(DIRECTORY_SEPARATOR, trim($data, DIRECTORY_SEPARATOR));
         }
+        $data = $this->trimEach($data);
         
         if ($this->isImmutable) {
             return new self(array_merge($this->atoms, $data), $this->isImmutable);
@@ -53,6 +56,17 @@ class Path
     
         $this->atoms = array_merge($this->atoms, $data);
         return $this;
+    }
+    
+    /**
+     * apply trim for each element of array
+     *
+     * @param $data
+     * @return array|string[]
+     */
+    private function trimEach($data)
+    {
+        return array_map(static function ($item) { return trim($item, DIRECTORY_SEPARATOR); }, $data);
     }
     
     
